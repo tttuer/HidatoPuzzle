f3a473b0f301e40f9976b65aae03e5e474d2ba2c
Jayyoung Yang <jay_y1711@hotmail.com>
2018-11-17 00:56:08 +0900

    Initial Commit

---
+++ HidatoPuzzle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HidatoPuzzle
+//
+//  Created by Jayyoung Yang on 17/11/2018.
+//  Copyright © 2018 Jayyoung Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
